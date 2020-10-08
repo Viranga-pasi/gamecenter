9ce88186cb68ace2143072ac8d965f12752b701e
Viranga Jayawardana <virangapasdindu4@gmail.com>
2020-10-08 23:25:54 +0530

    recorrect firebase

---
+++ src/Components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./HomePage.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <h1 className="heading">Game Center</h1>
+
+        <button className="hbtn">Sign Out</button>
+      </div>
+    </div>
+  );
+};
+export default Header;
